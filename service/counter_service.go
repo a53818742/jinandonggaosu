222a0cd17c7e052c822d4a9c50cd668a6aeaac5c
a53818742 <53818742@qq.com>
2022-09-01 11:05:04 +0800

    两个页面

--- service/counter_service.go
+++ service/counter_service.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	"wxcloudrun-golang/db/dao"
@@ -66,9 +67,9 @@ func GetUserInfo(w http.ResponseWriter, r *http.Request) {
 	Openid := r.Header.Get("X-Wx-Openid")
 	at := r.Header.Get("X-Wx-Cloudbase-Access-Token")
 	url := "https://api.weixin.qq.com/wxa/getopendata?openid=" + Openid + "&cloudbase_access_token=" + at
-	//payload := strings.NewReader("a=111")
+	payload := strings.NewReader("a=111")
 	res := &JsonResult{}
-	response, _ := http.Post(url, "application/x-www-form-urlencoded", nil)
+	response, _ := http.Post(url, "application/x-www-form-urlencoded", payload)
 
 	res.Code = 0
 	res.ErrorMsg = ""
