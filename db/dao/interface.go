3b8884e9ed383c664fd59cae6921eb1a25a869d6
a53818742 <53818742@qq.com>
2022-09-02 09:33:54 +0800

    两个页面

--- db/dao/interface.go
+++ db/dao/interface.go
@@ -6,9 +6,6 @@ import (
 
 // CounterInterface 计数器数据模型接口
 type CounterInterface interface {
-	GetCounter(id int32) (*model.CounterModel, error)
-	UpsertCounter(counter *model.CounterModel) error
-	ClearCounter(id int32) error
 	InsertCar(counter *model.WeihuapinCarInsert) error
 	UpdateCar(counter *model.WeihuapinCarUpdate) error
 	OverCar(counter *model.WeihuapinCarOver) error
