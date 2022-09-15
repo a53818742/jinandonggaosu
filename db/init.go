597fe91578b148e1e0a5765c074ed35e26ed069c
a53818742 <53818742@qq.com>
2022-09-15 14:05:56 +0800

    测试

--- db/init.go
+++ db/init.go
@@ -1,8 +1,10 @@
 package db
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
+	"reflect"
 	"time"
 
 	"gorm.io/driver/mysql"
@@ -11,6 +13,7 @@ import (
 )
 
 var dbInstance *gorm.DB
+var MySQLDB *sql.DB
 
 // Init 初始化数据库
 func Init() error {
@@ -49,47 +52,45 @@ func Init() error {
 	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	dbInstance = db
-
+	MySQLDB = sqlDB
 	fmt.Println("finish init mysql with ", source)
 
-	//f := dbInstance.Exec("select t1.userid,users.wechartid from (select * from record where starttime<='2022-09-01 05:00:00' and endtime >='2022-09-01 05:00:00')t1 LEFT JOIN users on t1.userid=users.ID")
-	//
-	//rows, err002 := f.Rows()
-	//if err002 != nil {
-	//	fmt.Println("Query ", err.Error())
-	//
-	//} else {
-	//	defer rows.Close()
-	//	columns, _ := rows.Columns()            //获取列的信息
-	//	count := len(columns)                   //列的数量
-	//	var values = make([]interface{}, count) //创建一个与列的数量相当的空接口
-	//	for i := 0; i < count; i++ {
-	//		var ii interface{} //为空接口分配内存
-	//		values[i] = &ii    //取得这些内存的指针，因后继的Scan函数只接受指针
-	//	}
-	//	ret := make([]map[string]interface{}, 0)
-	//	for rows.Next() {
-	//		err0 := rows.Scan(values...)  //开始读行，Scan函数只接受指针变量
-	//		m := map[string]interface{}{} //用于存放1列的 [键/值] 对
-	//		if err0 != nil {
-	//			panic(err)
-	//		}
-	//		for i, colName := range columns {
-	//			var rawValue = *(values[i].(*interface{})) //读出raw数据，类型为byte
-	//			if reflect.TypeOf(rawValue) == reflect.TypeOf([]byte{0}) {
-	//				b, _ := rawValue.([]byte)
-	//				v := string(b) //将raw数据转换成字符串
-	//				m[colName] = v //colName是键，v是值
-	//				fmt.Println("0=", colName, reflect.TypeOf(v), reflect.TypeOf(v).Name())
-	//			} else {
-	//				m[colName] = rawValue
-	//				fmt.Println("1=", colName, reflect.TypeOf(rawValue), reflect.TypeOf(rawValue).Name())
-	//			}
-	//
-	//		}
-	//		ret = append(ret, m) //将单行所有列的键值对附加在总的返回值上（以行为单位）
-	//	}
-	//}
+	rows, err002 := MySQLDB.Query("select t1.userid,users.wechartid from (select * from record where starttime<='2022-09-01 05:00:00' and endtime >='2022-09-01 05:00:00')t1 LEFT JOIN users on t1.userid=users.ID")
+	if err002 != nil {
+		fmt.Println("Query ", err.Error())
+
+	} else {
+		defer rows.Close()
+		columns, _ := rows.Columns()            //获取列的信息
+		count := len(columns)                   //列的数量
+		var values = make([]interface{}, count) //创建一个与列的数量相当的空接口
+		for i := 0; i < count; i++ {
+			var ii interface{} //为空接口分配内存
+			values[i] = &ii    //取得这些内存的指针，因后继的Scan函数只接受指针
+		}
+		ret := make([]map[string]interface{}, 0)
+		for rows.Next() {
+			err0 := rows.Scan(values...)  //开始读行，Scan函数只接受指针变量
+			m := map[string]interface{}{} //用于存放1列的 [键/值] 对
+			if err0 != nil {
+				panic(err)
+			}
+			for i, colName := range columns {
+				var rawValue = *(values[i].(*interface{})) //读出raw数据，类型为byte
+				if reflect.TypeOf(rawValue) == reflect.TypeOf([]byte{0}) {
+					b, _ := rawValue.([]byte)
+					v := string(b) //将raw数据转换成字符串
+					m[colName] = v //colName是键，v是值
+					fmt.Println("0=", colName, v, reflect.TypeOf(v), reflect.TypeOf(v).Name())
+				} else {
+					m[colName] = rawValue
+					fmt.Println("1=", colName, rawValue, reflect.TypeOf(rawValue), reflect.TypeOf(rawValue).Name())
+				}
+
+			}
+			ret = append(ret, m) //将单行所有列的键值对附加在总的返回值上（以行为单位）
+		}
+	}
 
 	return nil
 }
